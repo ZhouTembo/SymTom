20ca5a4fe4d4ecf93d01dabf9cbdbfddd70ea1bb
Mia Anderson <meicy101@gmail.com>
2020-01-30 05:18:49 -0600

    Finished?

--- Controllers/CreatePdfController.cs
+++ Controllers/CreatePdfController.cs
@@ -8,6 +8,7 @@ using iTextSharp.text.pdf;
 using SymTom.Models;
 using SymTom.Data;
 using Grpc.Core;
+using static iTextSharp.text.Font;
 
 namespace SymTom.Controllers
 {
@@ -28,9 +29,16 @@ namespace SymTom.Controllers
             Document pdfDoc = new Document(PageSize.A4, 25, 25, 25, 15);
             PdfWriter pdfWriter = PdfWriter.GetInstance(pdfDoc, Response.Body);
             pdfDoc.Open();
+
+
+            
+
             PdfPTable symptom = new PdfPTable(4); 
             {
+                Font hope = new Font(FontFamily.COURIER, 16, Font.BOLD);
+
                 symptom.AddCell("Location"); //col 1 row 1
+                
                 symptom.AddCell("Symptom"); //col 2 row 1
                 symptom.AddCell("Date"); //col 1 row 2
                 symptom.AddCell("Severity"); //col 2 row 2
@@ -45,10 +53,27 @@ namespace SymTom.Controllers
                 }
 
 
+
             }//declare new table
-                 //col 1 row 3
+             //col 1 row 3
+            string url = "https://doctorfreedompodcast.podbean.com/mf/web/4pmf3k/2-doctors-x-ray.jpg";
+            Image img = Image.GetInstance(url);
+            img.ScaleToFit(1000f, 1000f);
+            img.ScaleAbsolute(500f, 500f);
+
+
+            Font font = new Font(FontFamily.HELVETICA, 20, Font.BOLD);
+
+
+            Paragraph header= new Paragraph("List of Symptoms",font);
+            header.Alignment=Element.ALIGN_CENTER;
+            header.SetLeading(18,0);
+            
+            pdfDoc.Add(header);
+            pdfDoc.Add(Chunk.NEWLINE);
             pdfDoc.Add(symptom);
             pdfDoc.Add(new Paragraph("Remember to speak with your physician about these symptoms at your next appointment."));
+            pdfDoc.Add(img);
             pdfDoc.Close();
             
             
